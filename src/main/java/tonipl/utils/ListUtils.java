41314741f66488204315e6175f946b32b4cde8ef
tonipl <t.polo.lopez@gmail.com>
2021-04-02 14:01:19 +0200

    Spring Core dependency is added in order to assert values.

--- src/main/java/tonipl/utils/ListUtils.java
+++ src/main/java/tonipl/utils/ListUtils.java
@@ -2,37 +2,31 @@ package tonipl.utils;
 
 import java.util.List;
 
-import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.util.Assert;
 
 import tonipl.model.Common;
 
 public class ListUtils {
+    private static final String EXCEPTION_LIST_EMPTY = "List may not be empty";
 
     private ListUtils() {
     }
 
     public static void sortById(List<Common> listToSort) throws IllegalArgumentException {
-        if (CollectionUtils.isNotEmpty(listToSort)) {
-            listToSort.sort((common1, common2) -> common1.getId().compareTo(common2.getId()));
-        } else {
-            throw new IllegalArgumentException();
-        }
+        Assert.notEmpty(listToSort, EXCEPTION_LIST_EMPTY);
+
+        listToSort.sort((common1, common2) -> common1.getId().compareTo(common2.getId()));
     }
 
     public static void sortByCreationAuthor(List<Common> listToSort) throws IllegalArgumentException {
-        if (CollectionUtils.isNotEmpty(listToSort)) {
-            listToSort.sort((common1, common2) -> common1.getCreationAuthor().compareTo(common2.getCreationAuthor()));
-        } else {
-            throw new IllegalArgumentException();
-        }
+        Assert.notEmpty(listToSort, EXCEPTION_LIST_EMPTY);
+
+        listToSort.sort((common1, common2) -> common1.getCreationAuthor().compareTo(common2.getCreationAuthor()));
     }
 
     public static void sortByCreationDate(List<Common> listToSort) throws IllegalArgumentException {
-        if (CollectionUtils.isNotEmpty(listToSort)) {
-            listToSort.sort((common1, common2) -> common1.getCreationDate().compareTo(common2.getCreationDate()));
+        Assert.notEmpty(listToSort, EXCEPTION_LIST_EMPTY);
 
-        } else {
-            throw new IllegalArgumentException();
-        }
+        listToSort.sort((common1, common2) -> common1.getCreationDate().compareTo(common2.getCreationDate()));
     }
 }
