8aa32684a7e6a72c0012c79259599c0f3519b83d
tonipl <t.polo.lopez@gmail.com>
2021-05-16 13:56:38 +0200

    Tests for common boolean utility of objects. A new dependency is added in order to test by different params when it is expected the same result.

---
+++ src/test/java/tonipl/utils/ItTest.java
@@ -0,0 +1,222 @@
+package tonipl.utils;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ItTest {
+
+	private static final String NOT_EMPTY_TEXT = "Foo";
+
+	@Test
+	public void testIsNullWithNullObject() {
+		final String text = null;
+		final boolean expected = true;
+		final boolean result = It.isNull(text);
+
+		assertThat(result, is(expected));
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { "", NOT_EMPTY_TEXT })
+	void testIsNullWithNotNullObject(String arg) {
+		final boolean expected = false;
+		final boolean result = It.isNull(arg);
+
+		assertThat(result, is(expected));
+	}
+
+	@Test
+	public void testIsNotNullWithNullObject() {
+		final String text = null;
+		final boolean expected = false;
+		final boolean result = It.isNotNull(text);
+
+		assertThat(result, is(expected));
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { "", NOT_EMPTY_TEXT })
+	void testIsNotNullWithNotNullObject(String arg) {
+		final boolean expected = true;
+		final boolean result = It.isNotNull(arg);
+
+		assertThat(result, is(expected));
+	}
+
+	@Test
+	public void testIsEmptyWithNullCollection() {
+		final Collection<Object> collection = null;
+		final boolean expected = true;
+		final boolean result = It.isEmpty(collection);
+
+		assertThat(result, is(expected));
+	}
+
+	@Test
+	public void testIsEmptyWithEmptyCollection() {
+		final Collection<Object> collection = new ArrayList<>();
+		final boolean expected = true;
+		final boolean result = It.isEmpty(collection);
+
+		assertThat(result, is(expected));
+	}
+
+	@Test
+	public void testIsEmptyWithNotEmptyCollection() {
+		final Collection<Object> collection = new ArrayList<>();
+		collection.add(NOT_EMPTY_TEXT);
+		final boolean expected = false;
+		final boolean result = It.isEmpty(collection);
+
+		assertThat(result, is(expected));
+	}
+
+	@Test
+	public void testIsNotEmptyWithNullCollection() {
+		final Collection<Object> collection = null;
+		final boolean expected = false;
+		final boolean result = It.isNotEmpty(collection);
+
+		assertThat(result, is(expected));
+	}
+
+	@Test
+	public void testIsNotEmptyWithEmptyCollection() {
+		final Collection<Object> collection = new ArrayList<>();
+		final boolean expected = false;
+		final boolean result = It.isNotEmpty(collection);
+
+		assertThat(result, is(expected));
+	}
+
+	@Test
+	public void testIsNotEmptyWithNotEmptyCollection() {
+		final Collection<Object> collection = new ArrayList<>();
+		collection.add(NOT_EMPTY_TEXT);
+		final boolean expected = true;
+		final boolean result = It.isNotEmpty(collection);
+
+		assertThat(result, is(expected));
+	}
+
+	@Test
+	public void testIsEmptyWithNullMap() {
+		final Map<Object, Object> map = null;
+		final boolean expected = true;
+		final boolean result = It.isEmpty(map);
+
+		assertThat(result, is(expected));
+	}
+
+	@Test
+	public void testIsEmptyWithEmptyMap() {
+		final Map<Object, Object> map = new HashMap<>();
+		final boolean expected = true;
+		final boolean result = It.isEmpty(map);
+
+		assertThat(result, is(expected));
+	}
+
+	@Test
+	public void testIsEmptyWithNotEmptyMap() {
+		final Map<Object, Object> map = new HashMap<>();
+		map.put(1, NOT_EMPTY_TEXT);
+
+		final boolean expected = false;
+		final boolean result = It.isEmpty(map);
+
+		assertThat(result, is(expected));
+	}
+
+	@Test
+	public void testIsNotEmptyWithNullMap() {
+		final Map<Object, Object> map = null;
+		final boolean expected = false;
+		final boolean result = It.isNotEmpty(map);
+
+		assertThat(result, is(expected));
+	}
+
+	@Test
+	public void testIsNotEmptyWithEmptyMap() {
+		final Map<Object, Object> map = new HashMap<>();
+		final boolean expected = false;
+		final boolean result = It.isNotEmpty(map);
+
+		assertThat(result, is(expected));
+	}
+
+	@Test
+	public void testIsNotEmptyWithNotEmptyMap() {
+		final Map<Object, Object> map = new HashMap<>();
+		map.put(1, NOT_EMPTY_TEXT);
+
+		final boolean expected = true;
+		final boolean result = It.isNotEmpty(map);
+
+		assertThat(result, is(expected));
+	}
+
+	@Test
+	public void testHasTextWithNullString() {
+		final String string = null;
+		final boolean expected = false;
+		final boolean result = It.hasText(string);
+
+		assertThat(result, is(expected));
+	}
+
+	@Test
+	public void testHasTextWithEmptyString() {
+		final String string = "";
+		final boolean expected = false;
+		final boolean result = It.hasText(string);
+
+		assertThat(result, is(expected));
+	}
+
+	@Test
+	public void testHasTextWithNotEmptyString() {
+		final String string = NOT_EMPTY_TEXT;
+		final boolean expected = true;
+		final boolean result = It.hasText(string);
+
+		assertThat(result, is(expected));
+	}
+
+	@Test
+	public void testHasNotTextWithNullString() {
+		final String string = null;
+		final boolean expected = true;
+		final boolean result = It.hasNotText(string);
+
+		assertThat(result, is(expected));
+	}
+
+	@Test
+	public void testHasNotTextWithEmptyString() {
+		final String string = "";
+		final boolean expected = true;
+		final boolean result = It.hasNotText(string);
+
+		assertThat(result, is(expected));
+	}
+
+	@Test
+	public void testHasNotTextWithNotEmptyString() {
+		final String string = NOT_EMPTY_TEXT;
+		final boolean expected = false;
+		final boolean result = It.hasNotText(string);
+
+		assertThat(result, is(expected));
+	}
+}
